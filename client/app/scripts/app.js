444228d14448bc2533798b588e7b64e385d127ec
Pulak Choudhury <pulak.pijush@object-frontier.com>
2016-12-02 18:17:28 +0530

    MRBP project base set up

---
+++ client/app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mrbpApp
+ * @description
+ * # mrbpApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mrbpApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mrbpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mrbpApp
+ */
+angular.module('mrbpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
