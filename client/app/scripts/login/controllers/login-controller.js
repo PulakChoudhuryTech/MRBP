0497c6faa9077b33425d0a916919bb0f06d6dc2c
Nithin <nithin.rayaroth1@gmaill.com>
2016-12-07 00:51:16 +0530

    login & register ui completed

--- client/app/scripts/login/controllers/login-controller.js
+++ client/app/scripts/login/controllers/login-controller.js
@@ -10,9 +10,23 @@
 angular.module('mrbpApp')
   .controller('LoginController', ['MrbpModelService', function (MrbpModelService) {
 
+    var init = function init() {
+        $('.toggle').on('click', function() {
+            $('.container').stop().addClass('active');
+            // $(this).css({'height': '210px'});
+        });
+
+        $('.close').on('click', function() {
+            $('.container').stop().removeClass('active');
+            // $('.toggle').css({'height': '140px'});
+        });
+    };
+
     MrbpModelService.getUsers().then(function(response) {
         console.log(response);
     }, function(error) {
         console.log(error);
     });
+
+    init();
 }]);
