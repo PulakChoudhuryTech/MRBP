571ec88ca68e4e92f4162191712ad8a9ef8e9317
Nithin <nithin.rayaroth1@gmaill.com>
2016-12-08 01:30:30 +0530

    login ui changes & authentication API integration

--- client/app/scripts/login/controllers/login-controller.js
+++ client/app/scripts/login/controllers/login-controller.js
@@ -8,25 +8,46 @@
  * Controller of the login
  */
 angular.module('mrbpApp')
-  .controller('LoginController', ['MrbpModelService', function (MrbpModelService) {
+  .controller('LoginController', ['$state', '$localStorage', 'MrbpModelService', function ($state, $localStorage, MrbpModelService) {
+
+    var vm = this;
 
     var init = function init() {
-        $('.toggle').on('click', function() {
-            $('.container').stop().addClass('active');
-            // $(this).css({'height': '210px'});
-        });
+        vm.isRegisterForm = false;
+    };
 
-        $('.close').on('click', function() {
-            $('.container').stop().removeClass('active');
-            // $('.toggle').css({'height': '140px'});
+    vm.onToggleClick = function onToggleClick() {
+        vm.isRegisterForm = true;
+        $('.container').stop().addClass('active');
+    };
+
+    vm.onCloseClick = function onCloseClick() {
+        vm.isRegisterForm = false;
+        $('.container').stop().removeClass('active');
+    };
+
+    vm.doLogin = function doLogin() {
+        var loginModel = {
+            userName : vm.username,
+            password : vm.password
+        };
+        MrbpModelService.getUserProfile(loginModel).then(function(response) {
+            console.log(response);
+            if (response && response.data === 'Invalid User') {
+                return;
+            }
+            $localStorage.uid = response.data.id;
+            $state.go('root.home');
+        }, function(error) {
+            console.log(error);
         });
     };
 
-    MrbpModelService.getUsers().then(function(response) {
+    /*MrbpModelService.getUsers().then(function(response) {
         console.log(response);
     }, function(error) {
         console.log(error);
-    });
+    });*/
 
     init();
 }]);
--- client/app/scripts/login/module.js
+++ client/app/scripts/login/module.js
@@ -8,6 +8,7 @@ angular.module('login', [])
         .state('root.login', {
             url: '/login',
             controller: 'LoginController',
+            controllerAs: 'LoginCtrl',
             templateUrl: 'views/login/login.html'
         });
 });
